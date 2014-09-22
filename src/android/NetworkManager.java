0f4b031d9c7a06e0828e0ee74cc6b32452befdf7
Joe Bowser <bowserj@apache.org>
2014-09-22 10:27:10 -0700

    CB-7595: Android L changes the type from Mobile to Cellular, I'm pretty sure this isn't documented
    (cherry picked from commit 37c0e3ff75f512b769b85a666f9a875e017de78d)

--- src/android/NetworkManager.java
+++ src/android/NetworkManager.java
@@ -43,6 +43,9 @@ public class NetworkManager extends CordovaPlugin {
     public static final String WIMAX = "wimax";
     // mobile
     public static final String MOBILE = "mobile";
+    
+    // Android L calls this Cellular, because I have no idea! 
+    public static final String CELLULAR = "cellular";
     // 2G network types
     public static final String GSM = "gsm";
     public static final String GPRS = "gprs";
@@ -218,7 +221,7 @@ public class NetworkManager extends CordovaPlugin {
             if (type.toLowerCase().equals(WIFI)) {
                 return TYPE_WIFI;
             }
-            else if (type.toLowerCase().equals(MOBILE)) {
+            else if (type.toLowerCase().equals(MOBILE) || type.toLowerCase().equals(CELLULAR)) {
                 type = info.getSubtypeName();
                 if (type.toLowerCase().equals(GSM) ||
                         type.toLowerCase().equals(GPRS) ||
